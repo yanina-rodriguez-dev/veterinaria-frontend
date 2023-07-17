a709a4fd13f89d1157be3848bd9b0ec42a7d7c5e
Ana Santillan <anisantillan10@gmail.com>
2023-07-17 16:31:40 -0300

    se agregaron los estilos y la primera parte de servicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import './Servicios.css'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Servicios from './components/views/PaginaPrincipal/Servicios';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
- 
+
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Servicios" element={<Servicios></Servicios>} ></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
