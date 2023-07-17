dab7c0808a304386167e95fe0e968fdd5a47622e
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-16 23:57:51 -0300

    Agrego estructura de navbar funcionando y responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import Navbar from "./components/common/Menu"
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 
 function App() {
  
   return (
     <>
-      
+      <Navbar></Navbar>
     </>
   )
 }
