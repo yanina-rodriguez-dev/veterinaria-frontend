748ac50c4dae63b04fc092089fc715944ace2ea7
Ana Santillan <anisantillan10@gmail.com>
2023-07-17 17:08:47 -0300

    se agrego un contenedor a las imagenes y se agrego el titulo

--- src/components/views/PaginaPrincipal/Servicios.jsx
+++ src/components/views/PaginaPrincipal/Servicios.jsx
@@ -1,34 +1,36 @@
-import { Container } from "react-bootstrap";
 
 const Servicios = () => {
     return (
-        <section className="contenedor mainSection">
-            <div className="contenedor-imagen">
-            <img className="imagen"
-                src="https://images.unsplash.com/photo-1611173622933-91942d394b04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-                alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://images.unsplash.com/photo-1644675443401-ea4c14bad0e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://plus.unsplash.com/premium_photo-1677165324281-3497b194c168?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://plus.unsplash.com/premium_photo-1663045852526-adc089e870d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://plus.unsplash.com/premium_photo-1664304200289-be5b07afe66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://images.pexels.com/photos/6235653/pexels-photo-6235653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://images.pexels.com/photos/9341729/pexels-photo-9341729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        </div>
-        <div className="contenedor-imagen">
-            <img className="imagen" src="https://images.pexels.com/photos/7121954/pexels-photo-7121954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        </div>
+        <section className="mainSection">
+            <h1 className="display-4 ms-4 titulos">Servicios</h1>
+            <article className="contenedor">
+                <div className="contenedor-imagen">
+                    <img className="imagen"
+                        src="https://images.unsplash.com/photo-1611173622933-91942d394b04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+                        alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://images.unsplash.com/photo-1644675443401-ea4c14bad0e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://plus.unsplash.com/premium_photo-1677165324281-3497b194c168?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://plus.unsplash.com/premium_photo-1663045852526-adc089e870d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://plus.unsplash.com/premium_photo-1664304200289-be5b07afe66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://images.pexels.com/photos/6235653/pexels-photo-6235653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://images.pexels.com/photos/9341729/pexels-photo-9341729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                </div>
+                <div className="contenedor-imagen">
+                    <img className="imagen" src="https://images.pexels.com/photos/7121954/pexels-photo-7121954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                </div>
+            </article>
         </section>
     );
 };
