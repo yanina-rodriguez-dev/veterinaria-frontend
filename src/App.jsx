43ef9dc3e48977465022889b08b26bb44b42c51c
Ana Santillan <anisantillan10@gmail.com>
2023-07-18 03:58:28 -0300

    se agregaron las imagenes de los profesionales y se editaron los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-import './App.css'
+import './Profesionales.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Profesionales from './components/views/PaginaPrincipal/profesionales';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
  
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Profesionales" element={<Profesionales></Profesionales>} ></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
