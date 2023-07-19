69f6dd028125c3e9f437ad6a15b19861f133f5f2
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-19 20:27:02 -0300

    Modifico estetica de los items del footer y agrego logo al footer con los estilos para verse en circulo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,17 +1,25 @@
 import { Container, Nav} from "react-bootstrap";
-
+import logo from '../../assets/logo.png'
 
 const Footer = () => {
     return (
-        <Container className="bg-dark text-light">
-            <div className="fs-1 m-4 pt-3">O</div>
+        <div className="bg-dark text-light pt-3">
+            <Container>
+            <div className="logoFooter pt-3 text-center"><img src={logo} className="logoFooter " alt="logo" /></div>
             <div className="text-light ">
+                <aside className="mt-5 container text-center">
               <Nav.Link>Nosotros</Nav.Link>
+              <hr />
               <Nav.Link>Contacto</Nav.Link>
+              <hr />
               <Nav.Link>Ubicacion</Nav.Link>
+              <hr />
               <Nav.Link>Marcas</Nav.Link>
+              <hr />
               <Nav.Link>Productos</Nav.Link>
+              <hr />
               <Nav.Link>Terminos y condiciones</Nav.Link>
+                </aside>
               
                 
             </div>
@@ -20,7 +28,9 @@ const Footer = () => {
             </article>
 
             <hr />
-        </Container>
+            </Container>
+           
+        </div>
     );
 };
 
