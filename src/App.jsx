504bd96af1619fd60434d340c008140577d2f188
Ana Santillan <anisantillan10@gmail.com>
2023-07-19 05:43:19 -0300

    se agrego la ruta con react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-import './App.css'
+import './css/Registro.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Registro from './components/views/Registro';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
- 
+
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Registro" element={<Registro></Registro>} ></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
