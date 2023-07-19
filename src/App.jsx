78c369bdba2508dea20d20afcebf2d2614703c17
Ana Santillan <anisantillan10@gmail.com>
2023-07-19 04:39:07 -0300

    se agrego la ruta del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contacto from './components/views/Contacto';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
- 
+
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/Profesionales" element={<Contacto></Contacto>} ></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
