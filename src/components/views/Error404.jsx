cfce14c6bc4623aeec8a913b0cd00694ee995c80
Azul De Pol <azul.depol@gmail.com>
2023-07-20 12:20:45 +0100

    agrego boton para volver al inicio con estilos y centro el div

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,9 +1,13 @@
+import { Card, Button, Col, Row, Container } from "react-bootstrap";
 import React from "react";
 
 const Error404 = () => {
   return (
-      <section className="error d-flex flex-wrap align-items-center justify-content-center">
-          <iframe
+      <section className="error text-center">
+          <Container className="mt-5">
+            <Row>
+                <Col className="mt-5">
+                <iframe
             src="https://giphy.com/embed/0pPII0x2YJsUTHU5OT"
             width="480"
             height="270"
@@ -14,9 +18,13 @@ const Error404 = () => {
           <p>
             <a href="https://giphy.com/gifs/paws-dog-walking-pawprints-0pPII0x2YJsUTHU5OT"></a>
           </p>
-            <h1 className="f-title">Error 404</h1>
-            <h2 className="f-text">No quedan huellas de la pagina que estabas buscando...</h2>
-      </section>
+            <h1 className="f-title mt-5">Error 404</h1>
+            <h2 className="f-text mt-3">No quedan huellas de la pagina que estabas buscando...</h2>
+      <Button variant="default" className="btnError" to={"/src/components/views/Inicio.jsx"}>Volver al inicio</Button>
+                </Col>
+            </Row>
+          </Container>
+          </section>
   );
 };
 
