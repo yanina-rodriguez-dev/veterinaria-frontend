0fc45d991fcd8a24972dd1938be20d2017853754
maxivega0 <maximilianovega600@gmail.com>
2023-07-23 18:26:30 -0300

    validaciones en formularios con hookform

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -24,17 +24,43 @@ const Login = () => {
         <h1 className="titulos">Iniciar sesion</h1>
         <Form.Group className="mb-3 container" controlId="formBasicEmail">
           <Form.Label>Correo electronico</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            Nunca compartiremos tu contraseña con nadie mas.
+          <Form.Control
+            type="email"
+            placeholder="Ingrese un email"
+            {...register("email", {
+              required: "El email es un dato obligatorio",
+              pattern: {
+                value:
+                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                message:
+                  "El email debe tener el siguiente formato: 'mail@dominio.com'",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            Nunca compartiremos tu contraseña.<br></br>
+            {errors.email?.message}
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3 container" controlId="formBasicPassword">
           <Form.Label>Clave</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Ingrese una contraseña"
+            {...register("password", {
+              required: "El password es obligatorio",
+              pattern: {
+                value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                message: `La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.
+                 NO puede tener otros símbolos.`,
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
-
         <Button variant="primary" className="mx-5" type="submit">
           Acceder
         </Button>
