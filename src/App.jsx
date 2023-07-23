be9846d54c45e36e7ea9bb120ce2cd7adedd4c56
maxivega0 <maximilianovega600@gmail.com>
2023-07-23 18:03:43 -0300

    correcciones negrita en titulos banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import "./css/Administrador.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
-import Footer from "./components/common/Footer";
-// import Login from "./components/views/Login";
+import "./css/Administrador.css";
 import "./App.css";
 import "./Profesionales.css";
 import "./PlanesMarcas.css";
 import "./Profesionales.css";
-import Inicio from "./components/views/Inicio"
-import Contacto from "./components/views/Contacto"
-import Login from "./components/views/Login"
-import './css/Registro.css'
-import Registro from './components/views/Registro';
+import "./css/Registro.css";
 import Inicio from "./components/views/Inicio";
+import Contacto from "./components/views/Contacto";
+import Login from "./components/views/Login";
+import Registro from "./components/views/Registro";
 import Administrador from "./components/views/Administrador";
 import Menu from "./components/common/Menu";
 import AdmTurnos from "./components/views/AdmTurnos";
 import AdmPacientes from "./components/views/AdmPacientes";
-import { BrowserRouter , Route, Routes } from "react-router-dom";
 import Footer from "./components/common/Footer";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -28,11 +23,7 @@ function App() {
         <Menu></Menu>
         <Routes>
           <Route exact path="/" element={<Inicio></Inicio>}></Route>
-          <Route
-            exact
-            path="/login"
-            element={<Login></Login>}
-          ></Route>
+          <Route exact path="/login" element={<Login></Login>}></Route>
 
           <Route
             exact
