309796f858cb059c27403051eb43c59da247405b
Federico <federuedaleal@hotmail.com>
2023-07-23 23:53:42 -0300

    Se borraron los archivos que generan conflictos en el merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-
-// import './App.css'
 import './PlanesMarcas.css'
 import './Error404.css'
 import './AcercaDe.css'
-import Error404 from './components/views/Error404'
-import Marcas from './components/views/PaginaPrincipal/Marcas'
-import Planes from './components/views/PaginaPrincipal/Planes'
 import "bootstrap/dist/css/bootstrap.min.css";
 import AcercaDeNosotros from './components/views/AcercaDeNosotros'
 
@@ -13,9 +8,6 @@ function App() {
  
   return (
     <>
- {/* <Planes></Planes> */}
-{/* <Marcas></Marcas>  */}
-{/* <Error404></Error404> */}
 <AcercaDeNosotros></AcercaDeNosotros>
     </>
   );
