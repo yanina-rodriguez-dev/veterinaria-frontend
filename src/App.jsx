c8df08c76eb0a6844af22281946c919eb472c826
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2023-07-24 00:48:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import './PlanesMarcas.css'
 import './AcercaDe.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import AcercaDeNosotros from './components/views/AcercaDeNosotros'
+// import AcercaDeNosotros from './components/views/AcercaDeNosotros'
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
  
   return (
     <>
-<AcercaDeNosotros></AcercaDeNosotros>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
     </>
   );
 }
