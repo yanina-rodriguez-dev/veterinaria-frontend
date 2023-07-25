f7878fbe18846ac09acc6f1c78dd3e79aa64e3c9
Azul De Pol <azul.depol@gmail.com>
2023-07-26 00:13:12 +0100

    agrego seccion de productos a la pagina principal

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import login from "../../css/login.css";
+import login from "../../css/login.css?inline";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
