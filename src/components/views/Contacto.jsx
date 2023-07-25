2df865f11fdc69b5329d53c6b3143ecf7109a189
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-25 05:13:45 -0300

    Comienzo on la validacion de formularios, empece por el formulario de registro

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -1,23 +1,54 @@
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Contacto = () => {
+    const {register, handleSubmit, formState:{errors}, reset} = useForm();
+
+      const onSubmit = (usuario)=>{
+
+      }
     return (
         <div className="mt-5 mainSection">
             <h3 className="text-center titulos">Contáctanos!</h3>
             <hr />
             <div className="row justify-content-center w-100 ps-4">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                    <Form>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group className="mb-2">
                             <Form.Label>Nombre y Apellido</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Ingrese un nombre y apellido"
+                                maxLength={30}
+                                minLength={7}
+                                {
+                                    ...register('nombreContacto', {
+                                        required:'El nombre es un dato obligatorio.'
+                                    })
+                                }
                             />
+                            <Form.Text className="text-danger">
+                                {errors.nombreContacto?.message}
+                            </Form.Text>
                         </Form.Group>
                         <Form.Group className="mb-2">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Ingrese un email" />
+                            <Form.Control type="email" placeholder="Ingrese un email" 
+                            {
+                                ...register('email',{
+                                  required: 'El mail es un dato obligatorio',
+                                  pattern: {
+                                    value:/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/,
+                                    message: 'El email debe tener el siguiente formato email@dominio.com'
+                          
+                                  }  
+                                }
+                                )
+                            }
+                            />
+                              <Form.Text className="text-danger">
+                                {errors.email?.message}
+                            </Form.Text>
                         </Form.Group>
                         <Form.Group className="mb-2">
                             <Form.Label>Tipo de Consulta</Form.Label>
