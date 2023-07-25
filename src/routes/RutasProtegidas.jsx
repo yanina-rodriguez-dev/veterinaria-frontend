d02491e6cd0286008b58b56c63eb261e266130d9
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-25 02:54:51 -0300

    Agrego el componente RutasAdministrador y rutas protegidas con la logica para proteger las rutas

--- src/routes/RutasProtegidas.jsx
+++ src/routes/RutasProtegidas.jsx
@@ -1,16 +1,12 @@
 import { Navigate } from "react-router";
 
-
-const RutasProtegidas = ({children}) => {
- 
-   const usuarioLogueado = JSON.parse(sessionStorage.getItem('usuario')) || null; 
-   if(!usuarioLogueado){
-    return <Navigate to='/login'></Navigate>
-   }else{
-    return children
-   }
-    
-
+const RutasProtegidas = ({ children }) => {
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem("usuario")) || null;
+  if (!usuarioLogueado) {
+    return <Navigate to="/login"></Navigate>;
+  } else {
+    return children;
+  }
 };
 
 export default RutasProtegidas;
