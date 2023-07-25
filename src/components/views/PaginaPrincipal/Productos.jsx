bf6d2559bc3dc41717287b156c017c0e2e63cd61
Azul De Pol <azul.depol@gmail.com>
2023-07-26 00:18:33 +0100

    agrego estilos

--- src/components/views/PaginaPrincipal/Productos.jsx
+++ src/components/views/PaginaPrincipal/Productos.jsx
@@ -12,21 +12,22 @@ function Productos() {
         <Row>
         <Col xs={12} md={4} className="mb-4">
         <Card className="p-2 m-3 h-100">
-      <Card.Img variant="top" src="https://petplanetshop.com.ar/images/product_image/597/0?w=1000&h=1300&fit=contain" alt="comida para perros marca Eukanuba" />
+      <Card.Img className="card-img-top" src="https://petplanetshop.com.ar/images/product_image/597/0?w=1000&h=1300&fit=contain" alt="comida para perros marca Eukanuba" />
       <Card.Body className="text-verde">
         <Card.Title>Alimentos</Card.Title>
         <p className="italic">Para que coman como chanchos</p>
         <Card.Text className='mb-5'>
         Desde piensos y alimentos húmedos hasta snacks y golosinas, tenemos todo lo que tu mascota necesita para una alimentación equilibrada y nutritiva. 
         </Card.Text>
-        <Link to='/nosotros' className="btn btn-primary btn-lg my-4">
+        <br />
+        <Link to='/nosotros' className="btn btn-primary btn-lg my-4 mt-5">
             Ver más <FaPaw style={{ transform: "rotate(30deg)" }} />
           </Link>      </Card.Body>
     </Card>
         </Col>
         <Col xs={12} md={4} className="mb-4">
         <Card className="p-2 m-3 h-100">
-      <Card.Img variant="top" src="https://media.istockphoto.com/id/154956025/photo/dog-equipment.jpg?s=1024x1024&w=is&k=20&c=ZskDF-zIwnL1TDZGVO5cbcvHhH-CDAtIB4IErye0B1E=" alt="juguetes para animales" />
+      <Card.Img className="card-img-top" src="https://media.istockphoto.com/id/154956025/photo/dog-equipment.jpg?s=1024x1024&w=is&k=20&c=ZskDF-zIwnL1TDZGVO5cbcvHhH-CDAtIB4IErye0B1E=" alt="juguetes para animales" />
       <Card.Body className="text-verde">
         <Card.Title>Accesorios</Card.Title>
         <p className="italic">Para que sean felices, comiendo perdices</p>
@@ -41,7 +42,7 @@ function Productos() {
         </Col>
         <Col xs={12} md={4} className="mb-4">
         <Card className="p-2 m-3 h-100">
-      <Card.Img variant="top" src="https://petplanetshop.com.ar/images/product_image/362/0?w=1000&h=1300&fit=contain" alt="producto para pulgas de gatos"/>
+      <Card.Img className="card-img-top" src="https://petplanetshop.com.ar/images/product_image/362/0?w=1000&h=1300&fit=contain" alt="producto para pulgas de gatos"/>
       <Card.Body className="text-verde">
         <Card.Title>Salud</Card.Title>
         <p className="italic">Firmes como pata de elefante</p>
