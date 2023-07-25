467bc5ca0cbfa92d4985c88134d4958c89c4c29e
Ana Santillan <anisantillan10@gmail.com>
2023-07-25 05:45:53 -0300

    se agrego el gif y agregue estilos para que sea responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import Error404 from './components/views/Error404'
 import Marcas from './components/views/PaginaPrincipal/Marcas'
 import Planes from './components/views/PaginaPrincipal/Planes'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from './components/views/Inicio'
 
 function App() {
  
   return (
     <>
-{/* <Planes></Planes>
-<Marcas></Marcas> */}
-<Error404></Error404>
+<BrowserRouter>
+      <Routes>
+      <Route exact path="/Inicio" element={<Inicio></Inicio>} ></Route>
+        <Route exact path="/*" element={<Error404></Error404>} ></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,30 +1,18 @@
-import { Button, Col, Row, Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import React from "react";
+import error404 from '../../assets/error404.gif';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
   return (
-      <section className="error text-center">
-          <Container className="mt-5 text-center">
-            <Row>
-                <Col className="mt-5">
-                <iframe
-            src="https://giphy.com/embed/0pPII0x2YJsUTHU5OT"
-            width="480"
-            height="270"
-            frameBorder="0"
-            className="giphy-embed"
-            allowFullScreen
-            ></iframe>
-          <p>
-            <a href="https://giphy.com/gifs/paws-dog-walking-pawprints-0pPII0x2YJsUTHU5OT"></a>
-          </p>
-            <h1 className="f-title mt-5">Error 404</h1>
-            <h2 className="f-text mt-3">No quedan huellas de la pagina que estabas buscando...</h2>
-      <Button variant="default" className="btnError mt-3" to={"/src/components/views/Inicio.jsx"}>Volver al inicio</Button>
-                </Col>
-            </Row>
-          </Container>
-          </section>
+    <section className="error text-center p-5 mainSection">
+      <img src={error404} alt="error404" className="w-75" />
+      {/* <div>
+        <Link to="/Inicio">
+        <Button variant="default" className="btnError mt-3">Volver al inicio</Button>
+        </Link>
+      </div> */}
+    </section>
   );
 };
 
