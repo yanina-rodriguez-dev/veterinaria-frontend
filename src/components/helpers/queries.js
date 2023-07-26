f8a70dc19737a63c79bd51e0366d7068f3d4dc57
Federico <federuedaleal@hotmail.com>
2023-07-26 00:15:00 -0300

    Se da formato al código y limpia comentarios de Pacientes, nav y footer

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,5 +1,4 @@
 const URL_usuario = import.meta.env.VITE_API_USUARIOS;
-// const URL_turno = import.meta.env.VITE_API_TURNOS;
 const URL_paciente = import.meta.env.VITE_API_PACIENTES;
 
 export const iniciarSesion = async (usuario) => {
