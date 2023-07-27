ae82f01ada8fd14ea0539bde91bd7a94e9c422d8
maxivega0 <maximilianovega600@gmail.com>
2023-07-27 18:21:05 -0300

    formato al documento

--- src/components/views/Turnos/ItemTurno.jsx
+++ src/components/views/Turnos/ItemTurno.jsx
@@ -1,49 +1,53 @@
-import { TfiPencil, TfiTrash } from 'react-icons/tfi';
+import { TfiPencil, TfiTrash } from "react-icons/tfi";
 import { Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
-import { obtenerListaTurnos, borrarTurno } from '../../helpers/queries';
-import Swal from 'sweetalert2';
+import { Button } from "react-bootstrap";
+import { obtenerListaTurnos, borrarTurno } from "../../helpers/queries";
+import Swal from "sweetalert2";
 
 const ItemTurno = ({ turno, setTurnos }) => {
   const eliminarTurno = () => {
     Swal.fire({
-        title: 'Seguro que quiere borrar el turno?',
-        text: "El siguiente cambio no podra ser revertido",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#4D91CD',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, quiero borrar!',
-        cancelButtonText: 'Cancelar'
-    }).then((resultado) =>{
-    if (resultado.isConfirmed){
-        borrarTurno(turno.id).then((respuesta) =>{
-           if (respuesta.status === 200){
-               obtenerListaTurnos().then((respuesta)=>{
-                if (respuesta)
-    {
-        setTurnos(respuesta);
-    } else{
-        Swal.fire("Error", "Intente realizar esta operacion en unos minutos", "error");
-    }    
-          })
-          Swal.fire(
-            "Borrado!",
-            "El turno fue eliminado correctamente.",
-            "success"
-          )
-          }else{
+      title: "Seguro que quiere borrar el turno?",
+      text: "El siguiente cambio no podra ser revertido",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#4D91CD",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, quiero borrar!",
+      cancelButtonText: "Cancelar",
+    }).then((resultado) => {
+      if (resultado.isConfirmed) {
+        borrarTurno(turno.id).then((respuesta) => {
+          if (respuesta.status === 200) {
+            obtenerListaTurnos().then((respuesta) => {
+              if (respuesta) {
+                // actualizacion de tablaTurnos
+                setTurnos(respuesta);
+              } else {
+                Swal.fire(
+                  "Error",
+                  "Intente realizar esta operacion en unos minutos",
+                  "error"
+                );
+              }
+            });
+            Swal.fire(
+              "Borrado!",
+              "El turno fue eliminado correctamente.",
+              "success"
+            );
+          } else {
             Swal.fire({
-                title: "Lo siento!",
-                text: "El turno no pudo ser eliminado.",
-                icon: "error",
-                confirmButtonColor:" #4D91CD",
+              title: "Lo siento!",
+              text: "El turno no pudo ser eliminado.",
+              icon: "error",
+              confirmButtonColor: " #4D91CD",
             });
           }
-           })
-        }
-    })
-  }
+        });
+      }
+    });
+  };
   return (
     <tr>
       <td>{turno.id}</td>
@@ -53,10 +57,16 @@ const ItemTurno = ({ turno, setTurnos }) => {
       <td>{turno.nombreMascota}</td>
       <td>{turno.hora}</td>
       <td>{turno.fecha}</td>
-      <td className='text-center'>
-        <Link className="mx-3 p-2 px-2 mb-1 btn btn-primary" to={"editar-turno/" + turno.id}><TfiPencil /></Link>
-        <Button variant="danger mt-1" onClick={eliminarTurno}><TfiTrash /></Button>
-        
+      <td className="text-center">
+        <Link
+          className="mx-3 p-2 px-2 mb-1 btn btn-primary"
+          to={"editar-turno/" + turno.id}
+        >
+          <TfiPencil />
+        </Link>
+        <Button variant="danger mt-1" onClick={eliminarTurno}>
+          <TfiTrash />
+        </Button>
       </td>
     </tr>
   );
