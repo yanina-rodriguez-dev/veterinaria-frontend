d67530903a737a809c1fc26cb1023dcfa0c2a626
Federico <federuedaleal@hotmail.com>
2023-08-05 23:46:03 -0300

    Se agrega la función de paginación en el read de adm principal

--- src/components/views/Turnos/TablaTurnos.jsx
+++ src/components/views/Turnos/TablaTurnos.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 import { obtenerListaTurnos } from '../../helpers/queries';
 
 function TablaTurnos() {
+  const pageSize = 5;
   const [turnos, setTurnos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     obtenerListaTurnos().then((respuesta) => {
@@ -15,25 +18,56 @@ function TablaTurnos() {
     });
   }, []);
 
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const turnosPaginados = turnos.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <Table striped bordered size="sm" responsive>
-      <thead>
-        <tr>
-          <th>Paciente</th>
-          <th>Fecha</th>
-          <th>Hora</th>
-        </tr>
-      </thead>
-      <tbody>
-        {turnos.map((turno) => (
-          <tr key={turno._id}>
-            <td>{turno.nombreDueno}</td>
-            <td>{turno.fecha}</td>
-            <td>{turno.hora}</td>
+    <div>
+      <Table striped bordered size="sm" responsive>
+        <thead>
+          <tr>
+            <th>Paciente</th>
+            <th>Fecha</th>
+            <th>Hora</th>
           </tr>
+        </thead>
+        <tbody>
+          {turnosPaginados.map((turno) => (
+            <tr key={turno._id}>
+              <td>{turno.nombreDueno}</td>
+              <td>{turno.fecha}</td>
+              <td>{turno.hora}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination>
+        <Pagination.First onClick={() => handlePageChange(1)} />
+        <Pagination.Prev
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        />
+        {Array.from({ length: Math.ceil(turnos.length / pageSize) }).map((_, index) => (
+          <Pagination.Item
+            key={index + 1}
+            active={index + 1 === currentPage}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Pagination.Item>
         ))}
-      </tbody>
-    </Table>
+        <Pagination.Next
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={endIndex >= turnos.length}
+        />
+        <Pagination.Last onClick={() => handlePageChange(Math.ceil(turnos.length / pageSize))} />
+      </Pagination>
+    </div>
   );
 }
 
