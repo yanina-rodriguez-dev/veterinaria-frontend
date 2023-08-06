7aae2ea78660d94d6ad7451afbf40624b6d270e7
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-08-06 20:46:43 -0300

    Agrego nueva pagina de error 404 con imagen

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,16 +1,15 @@
 import { Button } from "react-bootstrap";
 import React from "react";
-import error404 from '../../assets/error404.gif';
+import errorCanva from '../../assets/404/errorCanva.png';
 import { Link } from 'react-router-dom';
 import "../../css/Error404.css"
 
 
 const Error404 = () => {
   return (
-    <section className="error text-center mb-5 mainSection">
-      <img src={error404} alt="error404" className="widthGif" />
+    <section className="error text-center my-0 mt-0 mainSection fondoError">
+      <img src={errorCanva} alt="error404" className="widthGif" />
       <div>
-      <h2 className="f-text mt-3 d-none d-sm-block text-dark">No quedan huellas de la pagina que estabas buscando...</h2>
         <Link to="/">
         <Button variant="default" className="btnError mt-3 text-light">Volver al inicio</Button>
         </Link>
